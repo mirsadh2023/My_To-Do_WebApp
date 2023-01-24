abb4593825b77875a3e6f6858909157f7770a3f7
mirsad <mirsad.hamidovic@gmail.com>
2023-01-24 22:17:20 +0100

    Change of place of text_input

--- web.py
+++ web.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -13,6 +15,9 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,5 +27,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
